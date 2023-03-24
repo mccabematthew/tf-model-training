fa604c23fe9b8f819f8cfcc403fa3c731f36f302
Matth <matthew.james.mccabe@outlook.com>
2023-03-23 22:53:43 -0500

    initial commit

---
+++ tfsd-celsius-to-fahrenheit.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+for i, c in enumerate(celsius_q):
+    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
