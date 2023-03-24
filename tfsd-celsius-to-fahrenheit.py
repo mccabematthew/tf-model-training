c3576b9bf0953b426409ca29d2a79e2d066d16e4
Matth <matthew.james.mccabe@outlook.com>
2023-03-23 22:54:58 -0500

    initial commit

--- tfsd-celsius-to-fahrenheit.py
+++ tfsd-celsius-to-fahrenheit.py
@@ -4,6 +4,7 @@ import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
+# training data: features, labels
 celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
