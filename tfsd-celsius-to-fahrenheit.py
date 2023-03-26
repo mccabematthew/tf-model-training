dd1322b9f1aa9e799c4f10ea84aab8e1d5780906
Matth <matthew.james.mccabe@outlook.com>
2023-03-26 00:36:57 -0500

    Created and trained sequential model, plotted to graph, printed result of prediction to float val 100.0

--- tfsd-celsius-to-fahrenheit.py
+++ tfsd-celsius-to-fahrenheit.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import logging
@@ -10,3 +11,21 @@ fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 for i, c in enumerate(celsius_q):
     print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+
+# a 'Sequential' model with dense layer as first layer
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=1, input_shape=[1])
+])
+
+# .1 learning rate
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.Adam(0.1))
+
+history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+print("Finished training the model")
+
+plt.xlabel('Epoch Number')
+plt.ylabel("Loss Magnitude")
+plt.plot(history.history['loss'])
+
+print(model.predict([100.0]))
